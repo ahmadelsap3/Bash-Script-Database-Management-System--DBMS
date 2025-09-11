396a77bf5645c4fe6c1970b91dd936d4a4bd0ba1
archie-bit <179378039+archie-bit@users.noreply.github.com>
2025-09-11 22:26:35 +0300

    Create function to create a new database, add /databases/ to .gitignore

--- main.sh
+++ main.sh
@@ -22,4 +22,43 @@ initialize() {
 }
 
 
+create_database() {
+    echo -e "${YELLOW}Enter Database Name:${NC}"
+    read -r db_name
+
+    if [[ ! $db_name =~ ^[a-zA-Z0-9_]+$ ]]; then
+        echo -e "${RED}Error: Database name must contain only letters, numbers, and underscores${NC}"
+        return 1
+    fi
+
+    if [ -d "$DB_DIR/$db_name" ]; then
+        echo -e "${RED}Database '$db_name' already exists!${NC}"
+    else
+        mkdir -p "$DB_DIR/$db_name"
+        echo -e "${GREEN}Database '$db_name' created successfully!${NC}"
+    fi
+}
+
+# Main menu function
+main_menu() {
+    while true; do
+        echo -e "\n${BLUE}======= DBMS Main Menu =======${NC}"
+        echo "1. Create Database"
+        #echo "2. List Databases"
+        #echo "3. Connect To Database"
+        #echo "4. Drop Database"
+        echo "5. Exit"
+        echo -e "${YELLOW}Enter your choice:${NC}"
+        read -r choice
+
+        case $choice in
+            1) create_database ;;
+            5)echo -e "${GREEN}Thank you for using Bash DBMS. Goodbye!${NC}"
+              exit 0 ;;
+            *) echo -e "${RED}Invalid option${NC}" ;;
+        esac
+    done
+}
+
 initialize
+main_menu
